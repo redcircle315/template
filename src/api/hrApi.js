f9426b605cef0415f720557c55a5fd333814285c
redci <redcircle315@naver.com>
2023-02-03 14:50:35 +0900

    프로젝트 시작

--- src/api/hrApi.js
+++ src/api/hrApi.js
@@ -1,5 +1,34 @@
 import axios from "axios";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export default axios.create({
     baseURL: 'http://localhost:9101'
 });
+/*
+
+//추가 API
+export const api = hrApi({
+    // reducerPath: "api"
+    reducerPath: "hrApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9104" }),
+    tagTypes: ["Count"],
+    endpoints: (builder) => ({
+        getCount: builder.query({
+            query: ({ name }) => `count/${name}`,
+            providesTags: (result, error, arg) => {
+                console.log(result, error, arg);
+                return [{ type: "Count", id: arg.name }];
+            }
+        }),
+        setCount: builder.mutation({
+            query: ({ name, value }) => {
+                return {
+                    url: `count/${name}`,
+                    method: "POST",
+                    body: { value }
+                };
+            },
+            invalidatesTags: (result, error, arg) => [{ type: "Count", id: arg.name }]
+        })
+    })
+});*/
